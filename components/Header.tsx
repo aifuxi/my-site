74f6305e1dd1699804a90df49240ff7a9e567866
aifuxi <aifuxi.js@gmail.com>
2022-07-19 22:25:16 +0800

    feat: 初始页面框架搭建

---
+++ components/Header.tsx
@@ -0,0 +1,91 @@
+import Link from 'next/link';
+import React from 'react';
+
+const Header: React.FC = () => {
+  return (
+    <div className="shadow-md navbar bg-base-100">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <label tabIndex={0} className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="w-5 h-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </label>
+          <ul
+            tabIndex={0}
+            className="p-2 mt-3 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52"
+          >
+            <li>
+              <a>Item 1</a>
+            </li>
+            <li tabIndex={0}>
+              <a className="justify-between">
+                Parent
+                <svg
+                  className="fill-current"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" />
+                </svg>
+              </a>
+              <ul className="p-2">
+                <li>
+                  <a>Submenu 1</a>
+                </li>
+                <li>
+                  <a>Submenu 2</a>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <a>Item 3</a>
+            </li>
+          </ul>
+        </div>
+        <a className="text-xl normal-case btn btn-ghost">F西的个人小站</a>
+      </div>
+      <div className="hidden navbar-center lg:flex">
+        <ul className="p-0 space-x-4 menu menu-horizontal">
+          <li>
+            <Link href="/">
+              <a>首页</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/dev_log">
+              <a>开发日志</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/about_me">
+              <a>关于我</a>
+            </Link>
+          </li>
+        </ul>
+      </div>
+      <div className="navbar-end">
+        <div className="avatar">
+          <div className="w-12 rounded-full">
+            <img src="https://placeimg.com/192/192/people" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
---
+++ pages/_app.tsx
@@ -0,0 +1,19 @@
+import '../styles/globals.css';
+import 'tailwindcss/tailwind.css';
+import type { AppProps } from 'next/app';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Header />
+      <div className="min-h-screen">
+        <Component {...pageProps} />
+      </div>
+      <Footer />
+    </>
+  );
+}
+
+export default MyApp;
